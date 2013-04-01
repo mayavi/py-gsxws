8a6d514696107e0944310286edab18ccd064c312
Filipp Lepalaan <f@230.to>
2013-04-01 17:30:18 +0300

    Added Content(), fetch_image()

--- gsxws.py
+++ gsxws.py
@@ -204,6 +204,16 @@ class GsxObject(object):
 
         return rd
 
+class Content(GsxObject):
+    def fetch_image(self, url):
+        '''
+        The Fetch Image API allows users to get the image file from GSX, 
+        for the content articles, using the image URL. 
+        The image URLs will be obtained from the image html tags in the data from all content APIs. 
+        '''
+        dt = self._make_type('ns3:fetchImageRequestType')
+        dt.imageRequest = {'imageUrl': url}
+        return self.submit('FetchImage', dt, 'contentResponse')
 
 class CompTIA(object):
     '''
