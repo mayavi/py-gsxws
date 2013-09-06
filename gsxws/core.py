f637233e97c1dee118097b25869a880df640ba8b
Filipp Lepalaan <f@230.to>
2013-09-06 18:57:45 +0300

    Show invalid value in validate()

--- gsxws/core.py
+++ gsxws/core.py
@@ -116,7 +116,7 @@ def validate(value, what=None):
     result = None
 
     if not isinstance(value, basestring):
-        raise ValueError('%s is not valid input')
+        raise ValueError('%s is not valid input' % value)
 
     rex = {
         'partNumber':       r'^([A-Z]{1,2})?\d{3}\-?(\d{4}|[A-Z]{1,2})(/[A-Z])?$',
